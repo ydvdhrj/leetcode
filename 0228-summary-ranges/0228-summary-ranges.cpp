14d7cadc9f489253a441ca91f41174561cdfee11
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-11 12:54:46 +0530

    Time: 3 ms (37.65%), Space: 8.3 MB (51.81%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        if(nums.empty()) return vector<string>();
+        vector<string> ans;
+        string s1 = to_string(nums[0]);
+        // s1+="->";
+        int count=0;
+        for(int i=1;i<nums.size();i++){
+            count++;
+            if(nums[i] != nums[i-1]+1){
+                if(count!=1){
+                s1+="->";
+                s1+=to_string(nums[i-1]);
+                }
+                count=0;
+                ans.push_back(s1);
+                s1=to_string(nums[i]);
+            }
+        }
+        if(count==0){
+            ans.push_back(s1);
+        }
+        else if(count!=0){
+            s1+="->";
+            s1+=to_string(nums[nums.size()-1]);
+            ans.push_back(s1);
+        }
+        return ans;
+    }
+};
