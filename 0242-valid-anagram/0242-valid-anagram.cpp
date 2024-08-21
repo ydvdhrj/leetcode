edabd0347db1526d8bf582b8e30531fd8f6066eb
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-21 09:13:29 +0530

    Time: 8 ms (63.54%), Space: 9.9 MB (22.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int> a;
+        if(s.size()!=t.size()) return false;
         for(char c:s){
             a[c]++;
         }
