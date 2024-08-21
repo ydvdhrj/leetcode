6350cf7055bb7d72c83e7d82056fd539db1906e5
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-21 09:58:20 +0530

    Time: 0 ms (100%), Space: 8.1 MB (73.61%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         int b=0,a=0,l=0,o=0,n=0;
-        for(int i=0;i<text.length();i++){
-            if(text[i]=='b') b++;
-            else if(text[i]=='a') a++;
-            else if(text[i]=='l'){
+        for(char c:text){
+            if(c=='b') b++;
+            else if(c=='a') a++;
+            else if(c=='l'){
                 l++;
             }
-            else if(text[i]=='o'){
+            else if(c=='o'){
                 o++;
             }
-            else if(text[i]=='n'){
+            else if(c=='n'){
                 n++;
             }
         }
-        int min = b;
-        l=l/2;
-        o=o/2;
-        if(a<min){
-            min=a;
-        }
-        if(n<min){
-            min = n;
-        }
-        if(l<min){
-            min=l;
-        }
-        if(o<min){
-            min=o;
-        }
+        // int min = b;
+        // l=l/2;
+        // o=o/2;
+        // if(a<min){
+        //     min=a;
+        // }
+        // if(n<min){
+        //     min = n;
+        // }
+        // if(l<min){
+        //     min=l;
+        // }
+        // if(o<min){
+        //     min=o;
+        // }
         // int sum = b+a+l/2+o/2+n;
-        return min;
+        return min({b,a,l/2,o/2,n});
 
     }
 };
