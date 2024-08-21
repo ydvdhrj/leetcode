20fd3814d6fe988a5ce89f6ec98f1e4b31906f95
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-21 08:51:24 +0530

    Time: 17 ms (50.66%), Space: 11.6 MB (23.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> m;
-        map<char,int> n;
+        unordered_map<char,int> m;
+        
         for(char i:magazine){
             m[i]++;
         }
         for(char i:ransomNote){
-            n[i]++;
-        }
-        for(auto i:n){
-            char key = i.first;
-            int value1 = i.second;
-            auto it = m.find(key);
-            if(it!=m.end()){
-                int value2 = it->second;
-                if(value1>value2){
-                    return false;
-                }
-            }
-            else{
+            if(m[i]==0){
                 return false;
             }
+            m[i]--;
         }
+        
         return true;
     }
 };
