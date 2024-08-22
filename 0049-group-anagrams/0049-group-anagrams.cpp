8aac9cb806d0fbaa88102a2e8a2dfd9bf743fde6
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-22 10:59:34 +0530

    Time: 28 ms (58.5%), Space: 25.2 MB (32.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-string getCount(const string& s) {
-        vector<int> count(26, 0);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
-        string countStr;
-        for (int i : count) {
-            countStr += to_string(i) + '#';
-        }
-        return countStr;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> m;
         for(string s:strs){
-            string temp = getCount(s);
-            // sort(temp.begin(),temp.end());
+            string temp = s;
+            sort(temp.begin(),temp.end());
             m[temp].push_back(s);
         }
         for(auto it:m){
