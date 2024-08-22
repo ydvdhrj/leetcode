b0cfbc4ce8ff563bcbc3dd459f62a0544a09c408
Dheeraj yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-22 11:29:16 +0530

    Time: 18 ms (52.87%), Space: 27 MB (65.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -17,9 +17,9 @@ public:
         // return ans;
 
         int ele = -1, count=0;
-        for(int i:nums){
-            if(count==0) ele = i;
-            count+= (i==ele)?1:-1;
+        for(int i=0;i<nums.size();i++){
+            if(count==0) ele = nums[i];
+            count+= (nums[i]==ele)?1:-1;
         }
         return ele;
     }
