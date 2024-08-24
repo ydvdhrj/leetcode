19bd8ebac2bde3673cdbc779d89925046ee9e45f
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-25 02:13:37 +0530

    Time: 69 ms (44.29%), Space: 32.8 MB (29.51%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -33,24 +33,21 @@ public:
 
         //another way
         sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         for(int i=0;i<nums.size()-2;i++){
-            
+            if(nums[i]>0) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int k=nums.size()-1;
             int j=i+1;
             while(j<k){
                 int sum = nums[i]+nums[j]+nums[k];
                 if(sum==0){
-                    vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[j]);
-                    temp.push_back(nums[k]);
-
-                    ans.insert(temp);
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
                     j++;
                     k--; 
-                    if(nums[j]==nums[j-1]) j++;
-                    if(nums[k]==nums[k+1]) k--;
+                    while(j<k &&nums[j]==nums[j-1]) j++;
+                    while(j<k &&nums[k]==nums[k+1]) k--;
                 }
                 else if(sum>0){
                     k--;
@@ -59,10 +56,7 @@ public:
                     j++;
                 }
             }
-            // int t = nums[i];
-            while(nums[i]==nums[i+1] && i<nums.size()-3) i++;
-
-
+            
         }
         // sort(ans.begin(),ans.end());
         // ans.erase(unique(ans.begin(),ans.end()),ans.end());
