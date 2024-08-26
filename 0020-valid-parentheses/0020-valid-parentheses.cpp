fcd0cf08715d5bd25bb12b1160415463d2b75827
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-08-26 13:21:21 +0530

    Time: 3 ms (40.7%), Space: 7.9 MB (24.38%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack <char> st;
+       for(int i=0;i<s.size();i++){
+           if(s[i]=='['||s[i]=='{'||s[i]=='('){
+               st.push(s[i]);
+           }
+           else{
+               if(!st.empty()){
+                   if(st.top()=='('&&s[i]==')'){
+                       st.pop();
+                   }
+                   else if(st.top()=='{'&&s[i]=='}'){
+                       st.pop();
+                   }
+                   else if(st.top()=='['&&s[i]==']'){
+                       st.pop();
+                   }
+                   else{
+                       return false;
+                   }
+               }
+               else{
+                   return false;
+               }
+           }
+       }
+       if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
