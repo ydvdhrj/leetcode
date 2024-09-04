4dc4fc49250b2bfa804a1adde039c6397e152aa6
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-09-04 14:10:53 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -16,20 +16,34 @@ public:
         // return ans;
 
         int l=0,r=n-1;
-        while(l<=r){
-            int m = l+(r-l)/2;
-            if(target==nums[m]) return m;
-            else if(target>nums[m]){
-                if(m+1<n && target<nums[m+1] ) return m+1;
-                else l=m+1;
-            } 
-            else {
-                if(m-1>=0 && target>nums[m-1]) return m;
-                else if(m==0) return m;
-                else r=m-1;
+        // while(l<=r){
+        //     int m = l+(r-l)/2;
+        //     if(target==nums[m]) return m;
+        //     else if(target>nums[m]){
+        //         if(m+1<n && target<nums[m+1] ) return m+1;
+        //         else l=m+1;
+        //     } 
+        //     else {
+        //         if(m-1>=0 && target>nums[m-1]) return m;
+        //         else if(m==0) return m;
+        //         else r=m-1;
+        //     }
+        // }
+        // return n;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                return m;
             }
         }
-        return n;
+
+        return l;
         
     }
 };
