81c3125bb0d6c2a3f996fe0ad2485877c2c90f9b
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-08 07:40:09 +0530

    Time: 6 ms (88.69%), Space: 16.9 MB (84.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,54 @@
 class Solution {
 public:
-    void replaceZero(int row,int col,vector<vector<int>> &matrix){
-        for(int j=0;j<matrix[0].size();j++){
-            matrix[row][j]=0;
-        }
-        for(int i=0;i<matrix.size();i++){
-            matrix[i][col]=0;
-        }
-    }
+// bruteforce approach
+    // void replaceZero(int row,int col,vector<vector<int>> &matrix){
+    //     for(int j=0;j<matrix[0].size();j++){
+    //         matrix[row][j]=0;
+    //     }
+    //     for(int i=0;i<matrix.size();i++){
+    //         matrix[i][col]=0;
+    //     }
+    // }
     void setZeroes(vector<vector<int>>& matrix) {
-        stack<pair<int,int>> row_col;
-        for(int row =0;row<matrix.size();row++){
-            for(int col = 0;col < matrix[0].size();col++){
-                if(matrix[row][col]==0){
-                    row_col.push({row,col});
+        //bruteforce approach
+        
+        // stack<pair<int,int>> row_col;
+        // for(int row =0;row<matrix.size();row++){
+        //     for(int col = 0;col < matrix[0].size();col++){
+        //         if(matrix[row][col]==0){
+        //             row_col.push({row,col});
+        //         }
+        //     }
+        // }
+        // while(!row_col.empty()){
+        //     auto top_pair = row_col.top();
+        //     replaceZero(top_pair.first,top_pair.second,matrix);
+        //     row_col.pop();
+        // }
+
+
+        //better approach
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        while(!row_col.empty()){
-            auto top_pair = row_col.top();
-            replaceZero(top_pair.first,top_pair.second,matrix);
-            row_col.pop();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
         }
     }
 };
