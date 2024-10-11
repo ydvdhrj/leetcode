d4d23528b2b7ef767b9516558dd7efdd1df21845
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-11 09:14:12 +0530

    Time: 5 ms (25.82%), Space: 10.2 MB (77.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) count0++;
-            if(nums[i]==1) count1++;
-            if(nums[i]==2) count2++;
-        }
+        // int count0=0,count1=0,count2=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0) count0++;
+        //     if(nums[i]==1) count1++;
+        //     if(nums[i]==2) count2++;
+        // }
         
-        for(int i=0;i<count0;i++) nums[i]=0;
-        for(int i=count0;i<count0+count1;i++) nums[i]=1;
-        for(int i=count0+count1;i<count0+count1+count2;i++) nums[i]=2;
+        // for(int i=0;i<count0;i++) nums[i]=0;
+        // for(int i=count0;i<count0+count1;i++) nums[i]=1;
+        // for(int i=count0+count1;i<count0+count1+count2;i++) nums[i]=2;
+
+
+        // Using Dutch flag algorithm
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
