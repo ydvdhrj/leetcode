d6c841b392f810f317c90d0481326d2d7cee6145
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-11 09:20:56 +0530

    Time: 92 ms (28.66%), Space: 96.2 MB (25.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int n = prices.size();
         int maxp = 0,min_val = prices[0];
         for(int i=0;i<n;i++){
-            if(prices[i]<min_val) min_val = prices[i];
+            min_val = min(min_val,prices[i]);
             maxp = max(prices[i] - min_val,maxp);
         }
         return maxp;
