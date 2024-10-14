dca936d18b243aa224ab5858f24aede1d1c37bff
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-14 10:57:16 +0530

    Time: 5 ms (5.16%), Space: 8.9 MB (56.73%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,35 +3,43 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         
-        // for(int i=0;i<n;i++){
-        //     for(int j=i;j<n;j++){
-        //         swap(matrix[i][j],matrix[j][i]);
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     reverse(matrix[i].begin(),matrix[i].end());
-        // }
-        for (int layer = 0; layer < n / 2; ++layer) {
-        int first = layer;
-        int last = n - 1 - layer;
+        // // for(int i=0;i<n;i++){
+        // //     for(int j=i;j<n;j++){
+        // //         swap(matrix[i][j],matrix[j][i]);
+        // //     }
+        // // }
+        // // for(int i=0;i<n;i++){
+        // //     reverse(matrix[i].begin(),matrix[i].end());
+        // // }
+        // for (int layer = 0; layer < n / 2; ++layer) {
+        // int first = layer;
+        // int last = n - 1 - layer;
         
-        for (int i = first; i < last; ++i) {
-            int offset = i - first;
-            int top = matrix[first][i];
+        // for (int i = first; i < last; ++i) {
+        //     int offset = i - first;
+        //     int top = matrix[first][i];
 
-            // Left -> Top
-            matrix[first][i] = matrix[last - offset][first];
+        //     // Left -> Top
+        //     matrix[first][i] = matrix[last - offset][first];
 
-            // Bottom -> Left
-            matrix[last - offset][first] = matrix[last][last - offset];
+        //     // Bottom -> Left
+        //     matrix[last - offset][first] = matrix[last][last - offset];
 
-            // Right -> Bottom
-            matrix[last][last - offset] = matrix[i][last];
+        //     // Right -> Bottom
+        //     matrix[last][last - offset] = matrix[i][last];
 
-            // Top -> Right
-            matrix[i][last] = top;
+        //     // Top -> Right
+        //     matrix[i][last] = top;
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
         }
-    }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
         
 
     }
