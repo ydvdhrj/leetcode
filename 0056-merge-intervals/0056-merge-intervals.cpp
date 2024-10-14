116a6dcaa8f27f17952a5f584fe1e2d8542b078c
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-14 11:35:39 +0530

    Time: 21 ms (73.7%), Space: 22.9 MB (88.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,18 +6,27 @@ public:
     //     });
     sort(intervals.begin(), intervals.end());
     vector<vector<int>> ans;
-    vector<int> prev = intervals[0];
-    for(int i=1;i<intervals.size();i++){
-        vector<int> interval  = intervals[i];
-        if(interval[0]<=prev[1]){
-            prev[1] = max(prev[1],interval[1]);
+    // vector<int> prev = intervals[0];
+    // for(int i=1;i<intervals.size();i++){
+    //     vector<int> interval  = intervals[i];
+    //     if(interval[0]<=prev[1]){
+    //         prev[1] = max(prev[1],interval[1]);
+    //     }
+    //     else{
+    //         ans.push_back(prev);
+    //         prev = interval;
+    //     }
+    // }
+    // ans.push_back(prev);
+
+    for(int i=0;i<intervals.size();i++){
+        if(ans.empty() || intervals[i][0]>ans.back()[1]){
+            ans.push_back(intervals[i]);
         }
         else{
-            ans.push_back(prev);
-            prev = interval;
+            ans.back()[1] = max(ans.back()[1],intervals[i][1]);
         }
     }
-    ans.push_back(prev);
     return ans;
     }
 };
