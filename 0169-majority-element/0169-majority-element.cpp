df38e4cbed9907a7ab140166c0e9e0ea7fcda84b
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-17 15:20:09 +0530

    Time: 22 ms (26.23%), Space: 28.1 MB (14.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,30 +3,30 @@ public:
     int majorityElement(vector<int>& nums) {
         //by using hash map
         
-        // unordered_map<int,int> m;
-        // for(int i:nums){
-        //     m[i]++;
-        // }
-        // int max=0,ans=0;
-        // for(auto it:m){
-        //     if(it.second>max){
-        //         ans=it.first;
-        //         max = it.second;
-        //     } 
-        // }
-        // return ans;
-
-
-        //another way
-        int ele = -1;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(count==0) ele = nums[i];
-            if(nums[i]==ele) count++;
-            else{
-                count--;
-            }
+        unordered_map<int,int> m;
+        for(int i:nums){
+            m[i]++;
         }
-        return ele;
+        int max=0,ans=0;
+        for(auto it:m){
+            if(it.second>max){
+                ans=it.first;
+                max = it.second;
+            } 
+        }
+        return ans;
+
+
+        // //another way
+        // int ele = -1;
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(count==0) ele = nums[i];
+        //     if(nums[i]==ele) count++;
+        //     else{
+        //         count--;
+        //     }
+        // }
+        // return ele;
     }
 };
