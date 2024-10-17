c7a2b353bd04950db1c800e8540f270c94912f0c
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-10-17 16:48:22 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,86 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        vector<int> ans;
-        for(int i:nums){
-            m[i]++;
+        //using hashmap
+        
+        // unordered_map<int,int> m;
+        // vector<int> ans;
+        // for(int i:nums){
+        //     m[i]++;
+        // }
+        // for(auto it:m){
+        //     if(it.second>nums.size()/3){
+        //         ans.push_back(it.first);
+        //     }
+        // }
+        // return ans;
+
+        // int ele1 = INT_MIN;
+        // int ele2 = INT_MIN;
+        // vector<int> ans;
+        // int count1=0,count2=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(count1==0 && nums[i]!=ele2){
+        //         ele1=nums[i];
+        //         count1++;
+        //     }
+        //     else if(count2==0 && nums[i]!=ele1){
+        //         ele2=nums[i];
+        //         count2++;
+        //     }
+        //     else if(nums[i]==ele1) count1++;
+        //     else if(nums[i]==ele2) count2++;
+        //     else{
+        //         count1--;
+        //         count2--;
+        //     }
+            
+        // }
+        // if(count1>=int(nums.size()/3)+1) ans.push_back(ele1);
+        // if(count2>=int(nums.size()/3)+1) ans.push_back(ele2);
+        // return ans;
+
+
+        int n = nums.size();
+        int cnt1 = 0, cnt2 = 0; // counts
+    int el1 = INT_MIN; // element 1
+    int el2 = INT_MIN; // element 2
+
+    // applying the Extended Boyer Moore's Voting Algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt1 == 0 && el2 != nums[i]) {
+            cnt1 = 1;
+            el1 = nums[i];
         }
-        for(auto it:m){
-            if(it.second>nums.size()/3){
-                ans.push_back(it.first);
-            }
+        else if (cnt2 == 0 && el1 != nums[i]) {
+            cnt2 = 1;
+            el2 = nums[i];
         }
-        return ans;
+        else if (nums[i] == el1) cnt1++;
+        else if (nums[i] == el2) cnt2++;
+        else {
+            cnt1--, cnt2--;
+        }
+    }
+
+    vector<int> ls; // list of answers
+
+    // Manually check if the stored elements in
+    // el1 and el2 are the majority elements:
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == el1) cnt1++;
+        if (nums[i] == el2) cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if (cnt1 >= mini) ls.push_back(el1);
+    if (cnt2 >= mini) ls.push_back(el2);
+
+    // Uncomment the following line
+    // if it is told to sort the answer array:
+    // sort(ls.begin(), ls.end()); //TC --> O(2*log2) ~ O(1);
+
+    return ls;
     }
 };
