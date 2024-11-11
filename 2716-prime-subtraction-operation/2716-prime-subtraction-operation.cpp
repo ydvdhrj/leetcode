1b090cb32991a517235e87fb1ac4a42f88c75717
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-11-11 10:03:14 +0530

    Time: 94 ms (7.25%), Space: 26.7 MB (97.12%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -8,11 +8,22 @@ private:
         return ans;
     }
     bool isPrime(int n){
-        if(n<=1) return false;
-        for(int i=2;i<n;i++){
-            if(n%i==0) return false;
-        }
-        return true;
+        // if(n<=1) return false;
+        // for(int i=2;i<n;i++){
+        //     if(n%i==0) return false;
+        // }
+        // return true;
+
+        if (n <= 1)
+		return false;
+     	if (n == 2 || n == 3)
+	     	return true;
+	    if (n % 2 == 0 || n % 3 == 0)
+		  return false;
+	    for (int i = 5; i <= sqrt(n); i = i + 6)
+		   if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+	    return true;
     }
     int primeDiff(int num, int prev){
         int mini = num;
