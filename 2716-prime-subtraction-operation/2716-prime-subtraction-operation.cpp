12c811436140dac9e0e3a222397953f5b3ef9257
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2024-11-11 10:12:37 +0530

    Time: 0 ms (100%), Space: 26.8 MB (87.22%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,11 +1,11 @@
 class Solution {
 private:
     bool isSorted(vector<int>& nums, int n){
-        bool ans = true;
+        // bool ans = true;
         for(int i=1;i<n;i++){
-            if(nums[i]<=nums[i-1]) ans = false;
+            if(nums[i]<=nums[i-1]) return false;
         }
-        return ans;
+        return true;
     }
     bool isPrime(int n){
         // if(n<=1) return false;
@@ -25,34 +25,55 @@ private:
 			return false;
 	    return true;
     }
-    int primeDiff(int num, int prev){
-        int mini = num;
-        for(int i=2;i<num;i++){
-            if(isPrime(i)){
-                int temp = num-i;
-                if(temp>prev) mini = temp;
-                else break;
-            }
-        }
-        return mini;
-    }
+    // int primeDiff(int num, int prev){
+    //     int mini = num;
+    //     for(int i=2;i<num;i++){
+    //         if(isPrime(i)){
+    //             int temp = num-i;
+    //             if(temp>prev) mini = temp;
+    //             else break;
+    //         }
+    //     }
+    //     return mini;
+    // }
 public:
     bool primeSubOperation(vector<int>& nums) {
         int n = nums.size();
         if(isSorted(nums,n)) return true;
-        bool sol = false;
-        for(int i=0;i<n;i++){
-            if(i==0){
-                nums[i]=primeDiff(nums[i],0);
-            }
-            else{
-                nums[i]=primeDiff(nums[i],nums[i-1]);
-            }
-            if(isSorted(nums,n)){
-                sol = true;
+        // bool sol = false;
+        // for(int i=0;i<n;i++){
+        //     if(i==0){
+        //         nums[i]=primeDiff(nums[i],0);
+        //     }
+        //     else{
+        //         nums[i]=primeDiff(nums[i],nums[i-1]);
+        //     }
+        //     if(isSorted(nums,n)){
+        //         sol = true;
+        //         break;
+        //     }
+        // }
+        for(int i = nums[0] - 1 ; i > 1; i--)
+        {
+            if(isPrime(i)){
+                nums[0] -= i;
                 break;
             }
         }
-        return sol;
+
+        for(int i=1;i<n;i++){
+            int k = nums[i] - nums[i-1];
+            if(k<=0) return false;
+            for(int j=k-1;j>1;j--)
+            {
+                if(isPrime(j))
+                {
+                    nums[i]-=j;
+                    break;
+                }
+            }
+
+        }
+        return true;
     }
 };
