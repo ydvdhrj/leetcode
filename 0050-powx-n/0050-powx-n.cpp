38767e94691a33af12d6553cca43e6bb2e0cdd0f
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-01-29 10:44:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -13,7 +13,7 @@ public:
         // }
         // return ans;
 
-
+        // By using Binary exponentiation 
         long long nn = n;
         double ans = 1.0;
         if(nn<0) nn = -1*nn;
@@ -29,5 +29,7 @@ public:
         }
         if(n<0) ans = (double)(1.0)/(double)(ans);
         return ans;
+
+        //by using recursion
     }
 };
