b31e15586c6ef67822be345866f447ca7520795e
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-01-29 10:58:11 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.98%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+double power(double b, long long e, double ans){
+    if(e==0) return ans;
+    if(e&1) return power(b,e-1,ans*b);
+    else return power(b*b,e/2,ans);
+}
     double myPow(double x, int n) {
         // if(n<0){
         //     x = 1/x;
@@ -14,22 +19,29 @@ public:
         // return ans;
 
         // By using Binary exponentiation 
-        long long nn = n;
-        double ans = 1.0;
-        if(nn<0) nn = -1*nn;
-        while(nn){
-            if(nn%2==1){
-                ans = ans*x;
-                nn--;
-            }
-            else{
-                x = x*x;
-                nn = nn/2;
-            }
-        }
-        if(n<0) ans = (double)(1.0)/(double)(ans);
-        return ans;
+
+        // long long nn = n;
+        // double ans = 1.0;
+        // if(nn<0) nn = -1*nn;
+        // while(nn){
+        //     if(nn%2==1){
+        //         ans = ans*x;
+        //         nn--;
+        //     }
+        //     else{
+        //         x = x*x;
+        //         nn = nn/2;
+        //     }
+        // }
+        // if(n<0) ans = (double)(1.0)/(double)(ans);
+        // return ans;
 
         //by using recursion
+        long long e=n;
+        double ans = 1.0;
+        if(n<0) e=-1LL*n;
+        double sol =  power(x,e,ans);
+        if(n<0) sol = (double)(1.0)/(double)(sol);
+        return sol;
     }
 };
