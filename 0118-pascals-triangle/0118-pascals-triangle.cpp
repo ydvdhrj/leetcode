c11d60bfd1aee52280400485a9bc6679c53bc63d
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-02-07 10:07:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (64.14%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    vector<int> generateRow(int row){
-        int ans=1;
-        vector<int> rowEle;
-        rowEle.push_back(1);
-        for(int col = 1;col<row;col++){
-            ans = ans*(row-col);
-            ans = ans/col;
-            rowEle.push_back(ans);
-        }
-        return rowEle;
-    }
+    // vector<int> generateRow(int row){
+    //     int ans=1;
+    //     vector<int> rowEle;
+    //     rowEle.push_back(1);
+    //     for(int col = 1;col<row;col++){
+    //         ans = ans*(row-col);
+    //         ans = ans/col;
+    //         rowEle.push_back(ans);
+    //     }
+    //     return rowEle;
+    // }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> sol;
-        for(int row=1;row<=numRows;row++){
-            sol.push_back(generateRow(row));
+    //     vector<vector<int>> sol;
+    //     for(int row=1;row<=numRows;row++){
+    //         sol.push_back(generateRow(row));
+    //     }
+    // return sol;
+
+    // using dp
+        vector<vector<int>> result;
+        vector<int> prevRow;
+        for(int i=0;i<numRows;i++){
+            vector<int> currRow(i+1,1);
+            for(int j=1;j<i;j++){
+                currRow[j] = prevRow[j]+prevRow[j-1];
+            }
+            result.push_back(currRow);
+            prevRow = currRow;
+
         }
-    return sol;
+        return result;
     }
 };
