f50c86e9f2b8c7e3956d2e8028d90af846788a0e
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-04-14 13:31:16 +0530

    Time: 15 ms (83.48%), Space: 12.5 MB (82.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s)==Counter(t)
+        # return Counter(s)==Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        my_dict = defaultdict(int)
+        for char in s:
+            my_dict[char] += 1
+        for char in t:
+            if my_dict[char]==0:
+                return False
+            my_dict[char] -= 1
+        
+        return True
