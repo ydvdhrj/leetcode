a984de16896728c626f0d05ae436753d42abd1d2
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-09-02 11:47:05 +0530

    Time: 0 ms (100%), Space: 32.4 MB (97.78%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -2,39 +2,33 @@ class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(points[i][0]<points[j][0]){
-                    swap(points[i],points[j]);
-                }
-                else if(points[i][0]==points[j][0]){
-                    if(points[i][1]>points[j][1]){
-                        swap(points[i],points[j]);
-                    }
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            int a = points[i][0];
-            int b = points[i][1];
-
-            cout<<a<<b<<",";
-        }
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(points[i][0]<points[j][0]){
+        //             swap(points[i],points[j]);
+        //         }
+        //         else if(points[i][0]==points[j][0]){
+        //             if(points[i][1]>points[j][1]){
+        //                 swap(points[i],points[j]);
+        //             }
+        //         }
+        //     }
+        // }
+        sort(points.begin(),points.end(), [](const auto& a, const auto& b){return a[0]==b[0]?a[1]>b[1]:a[0]<b[0]; });
+        reverse(points.begin(),points.end());
         int count=0;
         for(int i=0;i<n-1;i++){
             int y1 = points[i][1];
             int y2 = INT_MAX;
             for(int j=i+1;j<n;j++){
                 int y = points[j][1];
-                
                 if(y==y1){
                     count++;
                     break;
                 }
                 else if(y>y1 && y<y2){
                     count++;
-                    y2 = y;
-                    
+                    y2 = y; 
                 }
             }
         }
