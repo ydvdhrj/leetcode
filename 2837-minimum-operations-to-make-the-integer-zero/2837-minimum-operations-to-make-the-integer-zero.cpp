68da1f2fb52d9abd0160645ac2b781a1088b0c3b
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-09-05 21:23:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (100%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
-    int makeTheIntegerZero(int num1, int num2) {
-        
+    int makeTheIntegerZero(int n1, int n2) {
+        int k=1;
+        while(true){
+            long long x = (long long)n1 - (long long)k*n2;
+            // sum of k distinct power of 2 is always >=k 
+            if(x<k){ 
+                break;
+            }
+            int set_bit = __builtin_popcountll(x);
+            if(set_bit<=k){
+                return k;
+            }
+            k++;
+        }
+        return -1;
     }
 };
