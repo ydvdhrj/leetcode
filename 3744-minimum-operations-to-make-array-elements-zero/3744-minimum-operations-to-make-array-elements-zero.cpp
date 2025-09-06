7160a58d17afe5152e4e06250247491279a37e9a
Dheeraj Yadav <117137830+ydvdhrj@users.noreply.github.com>
2025-09-06 14:30:46 +0530

    Time: 116 ms (24.64%), Space: 207.4 MB (10.14%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
@@ -1,71 +1,42 @@
-// class Solution {
-// public:
-//     long long minOperations(vector<vector<int>>& queries) {
-//         int res = 0;
-//         int twos=0;
-//         bool ones=0;
-//         for(const auto& q : queries){
-//             vector<int> v;
-//             cout<<q[0]<<" "<<q[1];
-//             for(int i=q[1];i>=q[0];i--){
-//                 int op = floor(0.5*log2(i))+1;
-//                 if(op==2){
-//                     twos++;
-//                 }
-//                 if(op==1){
-//                     ones=1;
-//                 }
-//                 cout<<op<<" ";
-//                 v.push_back(op);
-//             }
-//             for(int j=0;j<v.size();j+=2){
-//                 if(j+1<v.size()){
-//                     if(v[j]==v[j+1]){
-//                     res+=v[j];
-//                     }
-//                     else if(v[j]>v[j+1]){
-//                         res+=v[j+1];
-//                         v.push_back(1);
-//                     }
-//                 }
-//                 else{
-//                     res+=v[j];
-//                 }
-                
-//             }
-//         }
-//         if(twos%2==1 && !ones){
-//             res-=1;
-//         }
-//         return res;
-//     }
-// };
-
+#define ll long long
 class Solution {
 public:
     long long minOperations(vector<vector<int>>& queries) {
-        long long ans = 0;
-        for (auto &q : queries) {
-            int l = q[0], r = q[1];
-            long long S = 0;
-            int dMax = 0;
+        // int res = 0;
+        // for(const auto& q : queries){
+        //     vector<int> v;
+        //     // cout<<q[0]<<" "<<q[1];
+        //     int sum=0;
+        //     for(int i=q[1];i>=q[0];i--){
+        //         int op = floor(0.5*log2(i))+1;
+        //         cout<<op<<" ";
+        //         sum+=op;
+        //     }
+            
+        //     res+=ceil(sum/2.0);
+            
+        // }
+        // return res;
 
-            for (int k = 1; k <= 31; k++) {
-                long long low = 1LL << (k - 1);
-                long long high = (1LL << k) - 1;
-                if (low > r) break;
-                long long a = max((long long)l, low);
-                long long b = min((long long)r, high);
-                if (a > b) continue;
-                long long cnt = b - a + 1;
-                int stepsForK = (k + 1) / 2;
-                S += cnt * stepsForK;
-                dMax = max(dMax, stepsForK);
+        // optmized way
+        ll ans=0;
+        for(auto it:queries){
+            ll l = it[0];
+            ll r = it[1];
+            ll prev = 1, operations = 0;
+            for(ll depth=1;depth<16;depth++){
+                ll curr = prev*4LL;
+
+                ll start=max(prev,l);
+                ll end=min(curr-1,r);
+
+                int range = end>=start?end-start+1:0;
+                operations += (range*depth);
+                prev = curr;
             }
-
-            long long ops = max((long long)dMax, (S + 1) / 2);
-            ans += ops;
+            ans += ceil(operations/2.0);
         }
         return ans;
+
     }
 };
